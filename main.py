4ab7937616eb8a0438232e50f0a5c4c9fc098605
idin-12 <trsrhdh@gmail.com>
2025-07-20 22:21:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ def main():
     """)
 
     is_hot = st.radio("Apakah suhu panas?", ("yes", "no")) == "yes"
-    window_closed = st.radio("Apakah jendela tertutup?_
+    window_closed = st.radio("Apakah jendela tertutup?", ("yes", "no")) == "yes"
+    someone_home = st.radio("Apakah ada orang di rumah?", ("yes", "no")) == "yes"
+
+    if st.button("Cek Status AC"):
+        if is_hot and window_closed and someone_home:
+            st.success("AC MENYALA")
+        else:
+            st.warning("AC MATI")
+
+if __name__ == "__main__":
+    main()
